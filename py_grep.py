cfae84251ce1d6d5c9b5bb8a62b031dc088f1078
Serrones <fabioserrones@gmail.com>
2019-08-24 21:24:35 -0300

    add exception

--- py_grep.py
+++ py_grep.py
@@ -4,13 +4,16 @@ def python_grep(text, word):
     
     path = path_file(text)
     
-    with open(path, 'r') as filename:
-        try:
+    try:
+        with open(path, 'r') as filename:
             for line_number, line in enumerate(filename):
                 if word in line:
                     print(f'line {line_number} -- '
                           f'{line}'
                     )
-        finally:
-            print('EOF')
             filename.close
+    except FileNotFoundError as error:
+        print('There is no file with this name')
+    finally:
+        print('Finished')
+
--- test/test_py_grep.py
+++ test/test_py_grep.py
@@ -3,17 +3,24 @@ import pytest
 from py_grep import python_grep
 
 
-def test_line_number_and_text(capsys):
+def test_line_number(capsys):
     python_grep('sample_test', 'fox')
     
     out, err = capsys.readouterr()
     
     assert 'line 3' in out
     
-def test_line(capsys):
+def test_word_in_file(capsys):
     python_grep('sample_test', 'fox')
 
     out, err = capsys.readouterr()
 
     assert 'fox' in out
     
+def test_should_raise_error_if_no_file(capsys):
+    message = 'There is no file with this name'
+    python_grep('foobar', 'fox')
+
+    out, err = capsys.readouterr()
+
+    assert message in out
